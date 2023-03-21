542ceda334eaf33831c83fd8a76ae6c36632bdb5
Antal Tony Tettinger <antal.tettinger@gmail.com>
2023-03-21 17:56:10 +0100

    feat: cresting JS state machine

--- 00-modeling/main.js
+++ 00-modeling/main.js
@@ -6,3 +6,61 @@ import '../style.css';
 
 // Also, come up with a simple way to "interpret" it, and
 // make it an object that you can `.send(...)` events to.
+
+const machine =  {
+  states: {
+    loading: {
+      on: {
+        loaded: {
+          state: 'playing'
+        }
+      }
+    },
+    playing: {
+      on: {
+        pause:
+        {
+          state: 'paused'
+        }
+      }
+    },
+    paused: {
+      on: {
+        play:
+        {
+          state: 'playing'
+        }
+      }
+    }
+  }
+}
+
+let currentState = {
+  value: 'loading'
+}
+
+const machineTransition = (state, event) => {
+  const newState = machine.states[state.value]?.on[event].state ?? state.value
+  return { value: newState }
+}
+
+const send = (event) => {
+  console.log('send')
+  currentState = machineTransition(currentState, event)
+  console.log('current',currentState)
+  return currentState
+}
+
+send('loaded')
+send('pause')
+send('play')
+
+const createPerson = (name, age, gender) => {
+  return {
+    name: name,
+    age: age,
+    gender: gender
+  }
+}
+
+console.log(createPerson('john','32','male'))
